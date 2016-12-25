744bc13617cac13e6a1e0e870e919a04476b103b
ZetrixWeb-Xamarin <xamarian.zetrixweb@gmail.comxamarian.zetrixweb@gmail.com>
2016-12-25 17:40:54 +0530

    First Commit
    Opne source project to learn for straters developed using Xamarin Studio

---
+++ Booking/Booking/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Booking.Helpers;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace Booking
+{
+    public partial class App : Application
+    {
+
+        public NavigationService Navigation { get; set; }
+
+        public static new App Current { get; private set; }
+
+        public bool Authenticated
+        {
+            get
+            {
+                return authenticated;
+            }
+
+            set
+            {
+                authenticated = value; OnPropertyChanged();
+            }
+        }
+
+        private bool authenticated;
+
+        public App()
+        {
+            InitializeComponent();
+
+            Current = this;
+            MainPage = new Booking.Pages.RootPage(true);
+            //MainPage = new Booking.Pages.LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+
+    }
+}
